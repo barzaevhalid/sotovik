4a3c359be46d53bb1b202fef15a62051187284c4
Khalid <barzaev97@gmail.com>
2025-12-28 15:03:01 +0100

    refactoring migrations

--- backend/migrations/0003_create_items_table.up.sql
+++ backend/migrations/0003_create_items_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE items (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    image_url VARCHAR,
+    image_url VARCHAR(255),
     price INTEGER NOT NULL,           -- цена в рублях, целое число
     wholesale_price INTEGER,          -- оптовая цена
     is_active BOOLEAN DEFAULT true,
